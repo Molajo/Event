10ae735c9aca89abddcf053950effbeb89b0459e
AmyStephen <AmyStephen@gmail.com>
2014-04-26 10:13:56 -0500

    Packaging

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,23 +6,11 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
+$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
+include_once $base . '/vendor/autoload.php';
 include_once __DIR__ . '/CreateClassMap.php';
 
-if (! defined('PHP_VERSION_ID')) {
-    $version = explode('.', phpversion());
-    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
-}
-
-$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
-$classmap = array();
-$classmap = createClassMap($base . '/vendor/commonapi/event', 'CommonApi\\Event\\');
-$results  = createClassMap(
-    $base . '/vendor/commonapi/exception',
-    'CommonApi\\Exception\\'
-);
-
-$classmap = array_merge($classmap, $results);
-
+$results  = createClassMap($base . '/Plugins/', 'Molajo\\Plugins\\');
 $classmap['Molajo\\Event\\Dispatcher']      = $base . '/Source/Dispatcher.php';
 $classmap['Molajo\\Event\\Event']           = $base . '/Source/Event.php';
 $classmap['Molajo\\Event\\EventDispatcher'] = $base . '/Source/EventDispatcher.php';
