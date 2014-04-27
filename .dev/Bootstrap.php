4a825a733b6fec3ae36fa3bd101ce8dd035784f8
AmyStephen <AmyStephen@gmail.com>
2014-04-27 05:40:09 -0500

    Data Gathering Process

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -8,7 +8,11 @@
  */
 $base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
 include_once $base . '/vendor/autoload.php';
-include_once __DIR__ . '/CreateClassMap.php';
+if (function_exists('CreateClassMap')) {
+} else {
+    include_once __DIR__ . '/CreateClassMap.php';
+}
+
 
 $results  = createClassMap($base . '/Plugins/', 'Molajo\\Plugins\\');
 $classmap['Molajo\\Event\\Dispatcher']      = $base . '/Source/Dispatcher.php';
