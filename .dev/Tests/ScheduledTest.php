e32beccd448f46e1e483f3604ad7f4224fa18b90
AmyStephen <AmyStephen@gmail.com>
2014-05-17 15:35:18 -0500

    Missing scheduled coverage

--- .dev/Tests/ScheduledTest.php
+++ .dev/Tests/ScheduledTest.php
@@ -9,7 +9,6 @@
 namespace Molajo\Event\Tests;
 
 use Molajo\Event\Scheduled;
-use CommonApi\Event\EventInterface;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -33,14 +32,16 @@ class ScheduledTest extends PHPUnit_Framework_TestCase
     /**
      * Set up
      *
+     * @covers  Molajo\Event\Scheduled::__construct
+     *
      * @return void
      * @since   1.0
      */
     protected function setUp()
     {
-        $event_name = null;
+        $event_name   = null;
         $return_items = array();
-        $data = array();
+        $data         = array();
 
         $this->scheduled = new Scheduled($event_name, $return_items, $data);
     }
@@ -48,6 +49,7 @@ class ScheduledTest extends PHPUnit_Framework_TestCase
     /**
      * Test Get Method
      *
+     * @covers  Molajo\Event\Scheduled::__construct
      * @covers  Molajo\Event\Scheduled::get
      *
      * @return  void
@@ -55,9 +57,9 @@ class ScheduledTest extends PHPUnit_Framework_TestCase
      */
     public function testGet()
     {
-        $scheduled_name   = 'Test';
-        $return_items = array('data1', 'data2');
-        $data         = array('data1' => 1, 'data2' => 2, 'data3' => 3);
+        $scheduled_name = 'Test';
+        $return_items   = array('data1', 'data2');
+        $data           = array('data1' => 1, 'data2' => 2, 'data3' => 3);
 
         $this->scheduled = new Scheduled($scheduled_name, $return_items, $data);
 
@@ -69,8 +71,33 @@ class ScheduledTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test Get Method
+     * Test Get Method Data array
      *
+     * @covers  Molajo\Event\Scheduled::__construct
+     * @covers  Molajo\Event\Scheduled::get
+     *
+     * @return  void
+     * @since   1.0
+     */
+    public function testGetData()
+    {
+        $scheduled_name = 'Test';
+        $return_items   = array('data1', 'data2');
+        $data           = array('data1' => 1, 'data2' => 2, 'data3' => 3);
+
+        $this->scheduled = new Scheduled($scheduled_name, $return_items, $data);
+
+        $this->assertEquals(1, $this->scheduled->get('data1'));
+        $this->assertEquals(2, $this->scheduled->get('data2'));
+        $this->assertEquals(3, $this->scheduled->get('data3'));
+
+        return;
+    }
+
+    /**
+     * Test Set Method
+     *
+     * @covers  Molajo\Event\Scheduled::__construct
      * @covers  Molajo\Event\Scheduled::set
      *
      * @return  void
@@ -78,9 +105,9 @@ class ScheduledTest extends PHPUnit_Framework_TestCase
      */
     public function testSet()
     {
-        $scheduled_name   = 'Test';
-        $return_items = array('data1', 'data2');
-        $data         = array('data1' => 1, 'data2' => 2, 'data3' => 3);
+        $scheduled_name = 'Test';
+        $return_items   = array('data1', 'data2');
+        $data           = array('data1' => 1, 'data2' => 2, 'data3' => 3);
 
         $this->scheduled = new Scheduled($scheduled_name, $return_items, $data);
 
@@ -92,19 +119,50 @@ class ScheduledTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test Get Exception
+     * Test Set Data Method
      *
-     * @covers  Molajo\Event\Scheduled::get
+     * @covers  Molajo\Event\Scheduled::__construct
+     * @covers  Molajo\Event\Scheduled::set
      *
-     * @expectedException \CommonApi\Exception\InvalidArgumentException
      * @return  void
      * @since   1.0
      */
+    public function testSetData()
+    {
+        $scheduled_name = 'Test';
+        $return_items   = array('data1', 'data2');
+        $data           = array('data1' => 1, 'data2' => 2, 'data3' => 3);
+
+        $this->scheduled = new Scheduled($scheduled_name, $return_items, $data);
+
+        $this->scheduled->set('data1', 10);
+        $this->scheduled->set('data2', 20);
+        $this->scheduled->set('data3', 30);
+
+        $this->assertEquals(10, $this->scheduled->get('data1'));
+        $this->assertEquals(20, $this->scheduled->get('data2'));
+        $this->assertEquals(30, $this->scheduled->get('data3'));
+
+        return;
+    }
+
+    /**
+     * Test Get Exception
+     *
+     * @covers                   Molajo\Event\Scheduled::__construct
+     * @covers                   Molajo\Event\Scheduled::get
+     *
+     * @expectedException \CommonApi\Exception\InvalidArgumentException
+     * @expectedExceptionMessage Event: Invalid Get Key: key_does_not_exist
+     *
+     * @return  void
+     * @since                    1.0
+     */
     public function testInvalidGetKey()
     {
-        $scheduled_name   = 'Test';
-        $return_items = array('data1', 'data2');
-        $data         = array('data1' => 1, 'data2' => 2, 'data3' => 3);
+        $scheduled_name = 'Test';
+        $return_items   = array('data1', 'data2');
+        $data           = array('data1' => 1, 'data2' => 2, 'data3' => 3);
 
         $this->scheduled = new Scheduled($scheduled_name, $return_items, $data);
 
@@ -114,19 +172,22 @@ class ScheduledTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test Get Exception
+     * Test Set Exception
      *
-     * @covers  Molajo\Event\Scheduled::get
+     * @covers                   Molajo\Event\Scheduled::__construct
+     * @covers                   Molajo\Event\Scheduled::set
      *
      * @expectedException \CommonApi\Exception\InvalidArgumentException
+     * @expectedExceptionMessage Event: Invalid Set Key: key_does_not_exist
+     *
      * @return  void
-     * @since   1.0
+     * @since                    1.0
      */
     public function testInvalidSetKey()
     {
-        $scheduled_name   = 'Test';
-        $return_items = array('data1', 'data2');
-        $data         = array('data1' => 1, 'data2' => 2, 'data3' => 3);
+        $scheduled_name = 'Test';
+        $return_items   = array('data1', 'data2');
+        $data           = array('data1' => 1, 'data2' => 2, 'data3' => 3);
 
         $this->scheduled = new Scheduled($scheduled_name, $return_items, $data);
 
